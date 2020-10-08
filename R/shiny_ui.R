046c84af343ec5fd531be120e1c9d8a7fcefa8ed
Adrien Ickowicz <adrien.ickowicz@csiro.au>
2020-10-08 17:01:30 +1100

    Slight UI twick

--- R/shiny_ui.R
+++ R/shiny_ui.R
@@ -170,7 +170,7 @@ ovva_shiny_ui_sidebyside <- function() {
                   tags$style(".showhide {border-radius: 20px; padding: 6px 9px; background: #668;} .showhide:hover {background: #668;} .showhide:focus {background: #668;}")
         ),
         shiny::wellPanel(
-            fluidRow(column(6, tags$h5("1. Select 1st clip"),
+            fluidRow(column(5, tags$h5("1. Select 1st clip"),
                             fluidRow(
                                 column(5, selectInput("season_sbs1", label = "Data set", choices = NULL),
                                        pickerInput(inputId = "game_table_dropdown_sbs1", label = "Games", choices = NULL, options = list(`actions-box` = TRUE), multiple = TRUE),
@@ -220,7 +220,16 @@ ovva_shiny_ui_sidebyside <- function() {
                                 )
                             )
             ),
-                     column(6, tags$h5("2. Select 2nd clip"),
+            column(2,tags$h5("3. Control timings"),
+                   uiOutput("player_controls_ui_sbs1", style = "margin-top: 12px;"),
+                   uiOutput("player_controls_ui_sbs2", style = "margin-top: 12px;"),
+                   #sliderInput("time_offset_sbs1", "Clip 1 time offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
+                   sliderInput("playback_rate_sbs12", "Playback rate:", min = 0.1, max = 1.5, value = 1.0, step = 0.1),
+                   #sliderInput("start_offset", "Clips start offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
+                   #sliderInput("time_offset_sbs12", "Clips time offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
+                   uiOutput("player_controls_ui_sbs12", style = "margin-top: 12px;")),
+                   
+                     column(5, tags$h5("2. Select 2nd clip"),
                             fluidRow(
                                 column(5, selectInput("season_sbs2", label = "Data set", choices = NULL),
                                        pickerInput(inputId = "game_table_dropdown_sbs2", label = "Games", choices = NULL, options = list(`actions-box` = TRUE), multiple = TRUE),
@@ -272,22 +281,22 @@ ovva_shiny_ui_sidebyside <- function() {
                      ))
         ),
         tags$hr(),
-        fluidRow(column(5, tags$div(id = "video_holder_sbs1",
+        fluidRow(column(6, tags$div(id = "video_holder_sbs1",
                                     ovideo::ov_video_player(id = "dv_player_sbs1", type = "local", controls = FALSE, poster = "data:image/gif,AAAA", style = "border: 1px solid black; width: 95%;", onloadstart = "set_vspinner_sbs1();", oncanplay = "remove_vspinner_sbs1();"),
                                     ovideo::ov_video_player(id = "dvyt_player_sbs1", type = "youtube", controls = FALSE, style = "border: 1px solid black; width: 95%; height: 480px; display:none;")), ## start hidden
                         #uiOutput("player_controls_ui_sbs1", style = "margin-top: 12px;"),
                         uiOutput("video_dialog_sbs1"),
                         DT::dataTableOutput("playstable_sbs1")
         ),
-        column(2,
-               uiOutput("player_controls_ui_sbs1", style = "margin-top: 12px;"),
-               uiOutput("player_controls_ui_sbs2", style = "margin-top: 12px;"),
-               #sliderInput("time_offset_sbs1", "Clip 1 time offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
-               sliderInput("playback_rate_sbs12", "Playback rate:", min = 0.1, max = 1.5, value = 1.0, step = 0.1),
-               #sliderInput("start_offset", "Clips start offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
-               #sliderInput("time_offset_sbs12", "Clips time offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
-               uiOutput("player_controls_ui_sbs12", style = "margin-top: 12px;")),
-        column(5, tags$div(id = "video_holder_sbs2",
+        # column(2,
+        #        uiOutput("player_controls_ui_sbs1", style = "margin-top: 12px;"),
+        #        uiOutput("player_controls_ui_sbs2", style = "margin-top: 12px;"),
+        #        #sliderInput("time_offset_sbs1", "Clip 1 time offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
+        #        sliderInput("playback_rate_sbs12", "Playback rate:", min = 0.1, max = 1.5, value = 1.0, step = 0.1),
+        #        #sliderInput("start_offset", "Clips start offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
+        #        #sliderInput("time_offset_sbs12", "Clips time offset:", min = -2.0, max = 2.0, value = 0.0, step = 0.1),
+        #        uiOutput("player_controls_ui_sbs12", style = "margin-top: 12px;")),
+        column(6, tags$div(id = "video_holder_sbs2",
                            ovideo::ov_video_player(id = "dv_player_sbs2", type = "local", controls = FALSE, poster = "data:image/gif,AAAA", style = "border: 1px solid black; width: 95%;", onloadstart = "set_vspinner_sbs2();", oncanplay = "remove_vspinner_sbs2();"),
                            ovideo::ov_video_player(id = "dvyt_player_sbs2", type = "youtube", controls = FALSE, style = "border: 1px solid black; width: 95%; height: 480px; display:none;")), ## start hidden
                #uiOutput("player_controls_ui_sbs2", style = "margin-top: 12px;"),
