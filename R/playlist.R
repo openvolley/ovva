1e69da1a6560e95b80ee3f6eaca9892aa9f1601c
Ben Raymond <ben@theraymonds.org>
2021-08-20 20:47:56 +1000

    refactor highlight handler code

--- R/playlist.R
+++ R/playlist.R
@@ -73,44 +73,23 @@ ovva_playlist_handler <- function() {
 
 #' The default handler for highlights within the shiny app
 #'
-#' You can cook up your own version of this function to suit your liking, and pass it as the `highlight_handler` parameter to \code{\link{ovva_shiny}}.
+#' `ovva_highlight_handler` provides functions that create highlight playlists. Highlights are judged according to the weighting scheme defined by `ovva_highlight_weighting`.
+#'
+#' @param clip_duration numeric: the maximum highlight clip length in seconds
+#' @param weights data.frame: a data.frame or tibble with columns `skill`, `evaluation_code`, and `highlight_weighting`. Any values provided here will override the corresponding defaults
+#'
+#' You can cook up your own version of this highlight handler function to suit your liking, and pass it as the `highlight_handler` parameter to \code{\link{ovva_shiny}}.
 #'
 #' @return A tibble, with columns 'skill', 'specific', and 'fun'. Each row defines a playlist entry that corresponds to specific match conditions. This playlist entry is referred to by the name given in `specific`, and applies to the skill in column `skill`. The corresponding entry in `fun` should be a function that takes three arguments (`x`, the play-by-play data; `team`, the team names to look for, and `player`, the player names to look for) and returns the subset of rows of `x` that correspond to the playlist conditions.
 #'
 #' @seealso \code{\link{ovva_shiny}}
 #'
 #' @export
-ovva_highlight_handler <- function() {
+ovva_highlight_handler <- function(clip_duration = 180) {
     ## start by defining the table of skills and specifics
 
     # whaouh factor points matrix
-
-    wfpm <- dplyr::tribble(~skill, ~evaluation_code, ~whaouhfactorpoints,
-                           "Attack", "#", 8,
-                           "Attack", "+", .1,
-                           "Attack", "-", .1,
-                           "Attack", "=", -2,
-                           "Serve",  "#", 8,
-                           "Block", "#", 8,
-                           "Block", "!", 1,
-                           "Set", "+", 1,
-                           "Set", "=", -2,
-                           "Freeball", "=", -2,
-                           "Freeball", "#", -2,
-                           "Freeball", "+", -2,
-                           "Freeball", "-", -2,
-                           "Freeball", "!", -2,
-                           "Freeball dig", "=", -2,
-                           "Freeball dig", "#", -2,
-                           "Freeball dig", "+", -2,
-                           "Freeball dig", "-", -2,
-                           "Freeball dig", "!", -2,
-                           "Freeball over", "=", -2,
-                           "Freeball over", "#", -2,
-                           "Freeball over", "+", -2,
-                           "Freeball over", "-", -2,
-                           "Freeball over", "!", -2
-                           )
+    wfpm <- ovva_highlight_weighting()
 
     out <- dplyr::tribble(~skill, ~specific,
                           "Highlights", "Game",
@@ -120,41 +99,78 @@ ovva_highlight_handler <- function() {
     ## now add the corresponding code for each one
     out$fun <- list(rep(NULL, nrow(out)))
     out$fun[[which(out$skill == "Highlights" & out$specific == "Game")]] <- function(x, team, player) {
-        x_tmp <- dplyr::left_join(x, wfpm, by = c("skill", "evaluation_code"))
-        x_tmp <- tidyr::drop_na(x_tmp, .data$video_time)
-        x_tmp <- dplyr::summarize(dplyr::group_by(x_tmp, .data$point_id), WFP = sum(.data$whaouhfactorpoints, na.rm = TRUE), duration = diff(range(.data$video_time, na.rm = TRUE)))
+        x_tmp <- dplyr::filter(dplyr::left_join(x, wfpm, by = c("skill", "evaluation_code")), !is.na(.data$video_time))
+        x_tmp <- dplyr::summarize(dplyr::group_by(x_tmp, .data$match_id, .data$point_id), WFP = sum(.data$highlight_weighting, na.rm = TRUE), duration = diff(range(.data$video_time, na.rm = TRUE)))
         x_tmp <- dplyr::arrange(x_tmp, -.data$WFP, -.data$duration)
         x_tmp$cumsumDur <- cumsum(x_tmp$duration)
-        pid_highlights <- sort(x_tmp$point_id[x_tmp$cumsumDur < 180 & x_tmp$cumsumDur > 0])
-        x[x$point_id %in% pid_highlights & !is.na(x$video_time), ]
+        x_tmp <- dplyr::filter(x_tmp, .data$cumsumDur > 0 & .data$cumsumDur <= clip_duration)
+        ## so x_tmp holds the match_id and point_id of the rallies that we want
+        dplyr::semi_join(dplyr::filter(x, !is.na(.data$video_time) & !is.na(.data$skill)), x_tmp, by = c("match_id", "point_id"))
     }
     out$fun[[which(out$skill == "Highlights" & out$specific == "Team")]] <- function(x, team, player) {
         if (missing(team) || is.null(team)) {
-            pid_highlights <- c()
+            return(x[c(), ])
         } else {
-            x_tmp <- dplyr::left_join(x, wfpm, by = c("skill", "evaluation_code"))
-            x_tmp$whaouhfactorpoints[x_tmp$team %eq% team] <- x_tmp$whaouhfactorpoints[x_tmp$team %eq% team]*2
-            x_tmp <- tidyr::drop_na(x_tmp, .data$video_time)
-            x_tmp <- dplyr::summarize(dplyr::group_by(x_tmp, .data$point_id), WFP = sum(.data$whaouhfactorpoints, na.rm = TRUE), duration = diff(range(.data$video_time, na.rm = TRUE)))
+            x_tmp <- dplyr::filter(dplyr::left_join(x, wfpm, by = c("skill", "evaluation_code")), !is.na(.data$video_time))
+            x_tmp$highlight_weighting[x_tmp$team %eq% team] <- x_tmp$highlight_weighting[x_tmp$team %eq% team] * 2
+            x_tmp <- dplyr::summarize(dplyr::group_by(x_tmp, .data$match_id, .data$point_id), WFP = sum(.data$highlight_weighting, na.rm = TRUE), duration = diff(range(.data$video_time, na.rm = TRUE)))
             x_tmp <- dplyr::arrange(x_tmp, -.data$WFP, -.data$duration)
             x_tmp$cumsumDur <- cumsum(x_tmp$duration)
-            pid_highlights <- sort(x_tmp$point_id[x_tmp$cumsumDur < 180 & x_tmp$cumsumDur > 0])
+            x_tmp <- dplyr::filter(x_tmp, .data$cumsumDur > 0 & .data$cumsumDur <= clip_duration)
+            dplyr::semi_join(dplyr::filter(x, !is.na(.data$video_time) & !is.na(.data$skill)), x_tmp, by = c("match_id", "point_id"))
         }
-        x[x$point_id %in% pid_highlights & !is.na(x$video_time),]
     }
     out$fun[[which(out$skill == "Highlights" & out$specific == "Player")]] <- function(x, team,  player) {
         if (missing(player) || is.null(player)) {
-            pid_highlights <- c()
+            return(x[c(), ])
         } else {
-            x_tmp <- dplyr::left_join(x, wfpm, by = c("skill", "evaluation_code"))
-            x_tmp$whaouhfactorpoints[x_tmp$player_name %eq% player] <- x_tmp$whaouhfactorpoints[x_tmp$player_name %eq% player]*2
-            x_tmp <- tidyr::drop_na(x_tmp, .data$video_time)
-            x_tmp <- dplyr::summarize(dplyr::group_by(x_tmp, .data$point_id), WFP = sum(.data$whaouhfactorpoints, na.rm = TRUE), duration = diff(range(.data$video_time, na.rm = TRUE)))
+            x_tmp <- dplyr::filter(dplyr::left_join(x, wfpm, by = c("skill", "evaluation_code")), !is.na(.data$video_time))
+            x_tmp$highlight_weighting[x_tmp$player_name %eq% player] <- x_tmp$highlight_weighting[x_tmp$player_name %eq% player] * 2
+            x_tmp <- dplyr::summarize(dplyr::group_by(x_tmp, .data$match_id, .data$point_id), WFP = sum(.data$highlight_weighting, na.rm = TRUE), duration = diff(range(.data$video_time, na.rm = TRUE)))
             x_tmp <- dplyr::arrange(x_tmp, -.data$WFP, -.data$duration)
             x_tmp$cumsumDur <- cumsum(x_tmp$duration)
-            pid_highlights <- sort(x_tmp$point_id[x_tmp$cumsumDur < 180 & x_tmp$cumsumDur > 0])
+            x_tmp <- dplyr::filter(x_tmp, .data$cumsumDur > 0 & .data$cumsumDur <= clip_duration)
+            dplyr::semi_join(dplyr::filter(x, !is.na(.data$video_time) & !is.na(.data$skill)), x_tmp, by = c("match_id", "point_id"))
         }
-        x[x$point_id %in% pid_highlights & !is.na(x$video_time),]
+    }
+    out
+}
+
+
+#' @export
+#' @rdname ovva_highlight_handler
+ovva_highlight_weighting <- function(weights) {
+    out <- dplyr::tribble(~skill, ~evaluation_code, ~highlight_weighting,
+                          "Attack", "#", 8,
+                          "Attack", "+", .1,
+                          "Attack", "-", .1,
+                          "Attack", "=", -2,
+                          "Serve",  "#", 8,
+                          "Block", "#", 8,
+                          "Block", "!", 1,
+                          "Set", "+", 1,
+                          "Set", "=", -2,
+                          "Freeball", "=", -2,
+                          "Freeball", "#", -2,
+                          "Freeball", "+", -2,
+                          "Freeball", "-", -2,
+                          "Freeball", "!", -2,
+                          "Freeball dig", "=", -2,
+                          "Freeball dig", "#", -2,
+                          "Freeball dig", "+", -2,
+                          "Freeball dig", "-", -2,
+                          "Freeball dig", "!", -2,
+                          "Freeball over", "=", -2,
+                          "Freeball over", "#", -2,
+                          "Freeball over", "+", -2,
+                          "Freeball over", "-", -2,
+                          "Freeball over", "!", -2
+                          )
+    if (!missing(weights) && !is.null(weights)) {
+        assert_that(is.data.frame(weights), all(c("skill", "evaluation_code", "highlight_weighting") %in% names(weights)))
+        weights$skill <- as.character(weights$skill)
+        weights$evaluation_code <- as.character(weights$evaluation_code)
+        out <- bind_rows(dplyr::anti_join(out, weights, by = c("skill", "evaluation_code")), weights)
     }
     out
 }
